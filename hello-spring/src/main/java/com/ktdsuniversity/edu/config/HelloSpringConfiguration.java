da27f3df84f514156d32272921fef9c4c64c0396
CodeMakers-KR <codemakers.kr@gmail.com>
2026-04-07 16:08:15 +0900

    세션 인터셉터 적용

--- hello-spring/src/main/java/com/ktdsuniversity/edu/config/HelloSpringConfiguration.java
+++ hello-spring/src/main/java/com/ktdsuniversity/edu/config/HelloSpringConfiguration.java
@@ -2,9 +2,12 @@ package com.ktdsuniversity.edu.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.ktdsuniversity.edu.config.interceptors.SessionInterceptor;
 
 // application.yml에서 작성할 수 없는 설정들을 적용하기 위한 Annotation
 // @Component 의 자식 Annotation
@@ -18,6 +21,28 @@ public class HelloSpringConfiguration implements
 		// WebMvc 설정을 위한 Configuration
 		// @EnableWebMvc Annotation 에서 적용하는 기본 설정들을 변경하기 위함.
 		WebMvcConfigurer {
+	
+	// Interceptor 등록 및 대상 URL 지정.
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		SessionInterceptor sessionInterceptor = new SessionInterceptor();
+		
+		registry.addInterceptor(sessionInterceptor)
+				.addPathPatterns("/**") // 모든 URL을 대상으로 sessionInterceptor를 수행해라!
+				.excludePathPatterns(
+						"/regist/check/duplicate/**", // 회원가입 이메일 중복 검사.
+						"/regist", // 회원가입 페이지 & 처리
+						"/login", // 로그인 페이지 & 처리
+						"/js/**", // static resources
+						"/css/**", // static resources
+						"/image/**", // static resources
+						"/", // 게시글 목록 조회
+						"/view/**", // 게시글 내용 조회
+						"/file/**" // 첨부파일 다운로드
+						) // sessionInterceptor가 적용되지 않을 URL 명시.
+				;
+		
+	}
 	
 	// configureViewResolvers 설정
 	//  spring.mvc.view.prefix, spring.mvc.view.suffix 재설정
